4e932aa96b8b698d935938daddbdacaa2965ef4a
wintermute28 <a28c@yandex.ru>
2024-10-21 12:59:29 +0300

    Add Header and Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+const Header = () => {
+  return (
+    <div>
+      <nav className="orange darken-3">
+        <div className="nav-wrapper">
+          <a href="" className="brand-logo">
+            Fortnite Shop
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a target="_blank" href="https://www.imdb.com/" rel="noreferrer">
+                IMDb
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://www.rottentomatoes.com/"
+                rel="noreferrer"
+              >
+                Rotten Tomatoes
+              </a>
+            </li>
+            <li>
+              <a
+                target="_blank"
+                href="https://www.metacritic.com/"
+                rel="noreferrer"
+              >
+                Metacritic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Shop.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Shop = () => {
+  return <main className="container content">Shop</main>;
+};
+
+export default Shop;
