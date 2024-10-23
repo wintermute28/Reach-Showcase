48d71aee6405808adbd061726c53f95f950fc709
wintermute28 <a28c@yandex.ru>
2024-10-23 18:26:24 +0300

    Add order state and card style

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import { API_KEY, API_URL } from "../config";
 import Preloader from "./Preloader";
 import GoodsList from "./GoodsList";
+import Cart from "./Cart";
 
 const Shop = () => {
   const [goods, setGoods] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [order, setOrder] = useState([]);
 
   useEffect(function getGoods() {
     fetch(API_URL, {
@@ -21,6 +23,7 @@ const Shop = () => {
 
   return (
     <main className="container content">
+      <Cart quantity={order.length} />
       {loading ? <Preloader /> : <GoodsList goods={goods} />}
     </main>
   );
